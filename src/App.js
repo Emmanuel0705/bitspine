bc00ea1d3b2f6edab78a96892e5abe9ef94a2442
Samuel Ajibola <samuel88783.so@gmail.com>
2022-07-21 21:15:45 +0100

    ---the comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Homepage from './pages/Homepage';
 import Dashboard from './pages/Dashboard';
-import files from './fakeFile';
 import { ethers } from 'ethers';
 import { CONSTANTS } from './constants';
 import Loader from './components/Loader';
@@ -154,26 +153,26 @@ const App = () => {
     }
   };
 
-  const getSupplyBalance = async () => {
-    try {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
+  // const getSupplyBalance = async () => {
+  //   try {
+  //     const provider = new ethers.providers.Web3Provider(ethereum);
+  //     const signer = provider.getSigner();
+  //     const address = await signer.getAddress();
 
-      const contract = new ethers.Contract(
-        CONSTANTS.CONTRACT_ADDRESS,
-        CONSTANTS.CONTRACT_ABI,
-        signer
-      );
+  //     const contract = new ethers.Contract(
+  //       CONSTANTS.CONTRACT_ADDRESS,
+  //       CONSTANTS.CONTRACT_ABI,
+  //       signer
+  //     );
 
-      const tx = (await contract.balanceOf(address)) / 1e8;
-      console.log(tx, '______________________TX');
-      setData(tx);
-      return contract;
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  //     const tx = (await contract.balanceOf(address)) / 1e8;
+  //     console.log(tx, '______________________TX');
+  //     setData(tx);
+  //     return contract;
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
   const enterMarket = async () => {
     try {
@@ -201,31 +200,31 @@ const App = () => {
     }
   };
 
-  const exitMarket = async () => {
-    try {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(
-        CONSTANTS.COMPTROLLER_CONTRACT_ADDRESS,
-        CONSTANTS.COMPT_ABI,
-        signer
-      );
+  // const exitMarket = async () => {
+  //   try {
+  //     const provider = new ethers.providers.Web3Provider(ethereum);
+  //     const signer = provider.getSigner();
+  //     const contract = new ethers.Contract(
+  //       CONSTANTS.COMPTROLLER_CONTRACT_ADDRESS,
+  //       CONSTANTS.COMPT_ABI,
+  //       signer
+  //     );
 
-      const markets = [CONSTANTS.CONTRACT_ADDRESS];
+  //     const markets = [CONSTANTS.CONTRACT_ADDRESS];
 
-      setLoader(true);
-      const tx = await contract.exitMarket(CONSTANTS.CONTRACT_ADDRESS, {
-        gasLimit: ethers.utils.hexlify(250000),
-        gasPrice: ethers.utils.hexValue(20000000000),
-      });
-      await tx.wait();
-      setLoader(false);
-      setReload(!reload);
-    } catch (error) {
-      setLoader(false);
-      console.log(error);
-    }
-  };
+  //     setLoader(true);
+  //     const tx = await contract.exitMarket(CONSTANTS.CONTRACT_ADDRESS, {
+  //       gasLimit: ethers.utils.hexlify(250000),
+  //       gasPrice: ethers.utils.hexValue(20000000000),
+  //     });
+  //     await tx.wait();
+  //     setLoader(false);
+  //     setReload(!reload);
+  //   } catch (error) {
+  //     setLoader(false);
+  //     console.log(error);
+  //   }
+  // };
 
   const getAccountLiquity = async () => {
     try {
@@ -248,27 +247,27 @@ const App = () => {
     }
   };
 
-  const getCollateralFactor = async () => {
-    try {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
-      const contract = new ethers.Contract(
-        CONSTANTS.COMPTROLLER_CONTRACT_ADDRESS,
-        CONSTANTS.COMPT_ABI,
-        signer
-      );
+  // const getCollateralFactor = async () => {
+  //   try {
+  //     const provider = new ethers.providers.Web3Provider(ethereum);
+  //     const signer = provider.getSigner();
+  //     const address = await signer.getAddress();
+  //     const contract = new ethers.Contract(
+  //       CONSTANTS.COMPTROLLER_CONTRACT_ADDRESS,
+  //       CONSTANTS.COMPT_ABI,
+  //       signer
+  //     );
 
-      console.log(address);
-      let { 1: collateralFactor } = await contract.markets(
-        CONSTANTS.CONTRACT_ADDRESS
-      );
-      collateralFactor = (collateralFactor / 1e18) * 100;
-      console.log({ collateralFactor }, 'TX_________________');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //     console.log(address);
+  //     let { 1: collateralFactor } = await contract.markets(
+  //       CONSTANTS.CONTRACT_ADDRESS
+  //     );
+  //     collateralFactor = (collateralFactor / 1e18) * 100;
+  //     console.log({ collateralFactor }, 'TX_________________');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   const borrow = async (amount) => {
     if (amount >= liquidity) {
       setMessage('You dont have enough Borrow limit to perform this operation');
@@ -301,63 +300,63 @@ const App = () => {
     }
   };
 
-  const borrowBalance = async () => {
-    try {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
-      const contract = new ethers.Contract(
-        CONSTANTS.CUSDT_CA,
-        CONSTANTS.CUSDT_ABI,
-        signer
-      );
+  // const borrowBalance = async () => {
+  //   try {
+  //     const provider = new ethers.providers.Web3Provider(ethereum);
+  //     const signer = provider.getSigner();
+  //     const address = await signer.getAddress();
+  //     const contract = new ethers.Contract(
+  //       CONSTANTS.CUSDT_CA,
+  //       CONSTANTS.CUSDT_ABI,
+  //       signer
+  //     );
 
-      console.log(address);
-      let balance = await contract.borrowBalanceCurrent(address);
-      // balance = balance / Math.pow(10, 18);
-      balance.wait();
-      console.log({ balance }, 'TX_________________');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //     console.log(address);
+  //     let balance = await contract.borrowBalanceCurrent(address);
+  //     // balance = balance / Math.pow(10, 18);
+  //     balance.wait();
+  //     console.log({ balance }, 'TX_________________');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const getAssetsIn = async () => {
-    try {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
-      const contract = new ethers.Contract(
-        CONSTANTS.COMPTROLLER_CONTRACT_ADDRESS,
-        CONSTANTS.COMPT_ABI,
-        signer
-      );
+  // const getAssetsIn = async () => {
+  //   try {
+  //     const provider = new ethers.providers.Web3Provider(ethereum);
+  //     const signer = provider.getSigner();
+  //     const address = await signer.getAddress();
+  //     const contract = new ethers.Contract(
+  //       CONSTANTS.COMPTROLLER_CONTRACT_ADDRESS,
+  //       CONSTANTS.COMPT_ABI,
+  //       signer
+  //     );
 
-      // console.log({ address });
-      // let assets = await contract.getAssetsIn(address);
-      console.log({ address });
-      let assets = await contract.getAllMarkets();
-      // balance = balance / Math.pow(10, 18);
-      console.log({ assets }, 'TX_________________');
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const uploadFileToContract = async (payload) => {
-    try {
-      console.log(payload, 'payl________-');
-      console.log('C__________________________');
-      const tx = await contract.addUserFile(
-        payload.uid,
-        payload.name,
-        payload.url,
-        payload.thumbnail
-      );
-      console.log(tx, 'uploaded');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //     // console.log({ address });
+  //     // let assets = await contract.getAssetsIn(address);
+  //     console.log({ address });
+  //     let assets = await contract.getAllMarkets();
+  //     // balance = balance / Math.pow(10, 18);
+  //     console.log({ assets }, 'TX_________________');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // const uploadFileToContract = async (payload) => {
+  //   try {
+  //     console.log(payload, 'payl________-');
+  //     console.log('C__________________________');
+  //     const tx = await contract.addUserFile(
+  //       payload.uid,
+  //       payload.name,
+  //       payload.url,
+  //       payload.thumbnail
+  //     );
+  //     console.log(tx, 'uploaded');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <div className="h-screen bg-[#011936]">
       <>{loader && <Loader />}</>
